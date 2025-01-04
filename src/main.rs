480785ecf422cb9e2a93934dfe9e82f213d9c02e
H-Chris233 <3475310230@qq.com>
2025-01-04 19:12:20 +0800

    delete for dev

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,20 @@
 #![no_main]
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println("Hello World!");
-    println("This is a simple kernel written in Rust.");
-    loop {}
-    
-}
-
-fn print(s: &str) {
-    for byte in s.bytes() {
-        print_byte(byte);
-    }
-}
-
-fn println(s: &str) {
-    print(s);
-    print_byte(b'\n');
-}
-
-fn print_byte(byte: u8) {
     let vga_buffer = 0xb8000 as *mut u8;
-    static mut COLUMN: u32 = 0;
-    static mut ROW: u32 = 0;
-
-    unsafe {
-        match byte {
-            b'\n' => {
-                ROW += 1;
-                COLUMN = 0;
-            }
-            byte => {
-                let color_byte = 0xb;
-                let row = ROW;
-                let column = COLUMN;
-
-                let offset = 2 * (row * 80 + column);
-                *vga_buffer.offset(offset as isize) = byte;
-                *vga_buffer.offset(offset as isize + 1) = color_byte;
-
-                COLUMN += 1;
-            }
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+    
+    loop {}
+    
 }
 
 
